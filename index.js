efac2cca5d43f2b0718393e108015e2d16a34e21
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-02-03 22:13:48 +0100

    added some js effects

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const form = document.querySelector('.form');
 const userEmail = document.querySelector('.email');
 const formBtn = document.querySelector('.form-btn');
 const upBtn = document.querySelector('.up-btn');
+const cards = document.querySelectorAll('.fade-in');
 
 // function to submit form
 const submitForm = () => {
@@ -36,6 +37,34 @@ const showTopBtn = () => {
 const goToTop = () => {
   window.scrollTo(0, 0);
 };
+
+// fade in options
+cardOptions = {
+  threshold: 0,
+  rootMargin: '0px 0px -250px 0px',
+};
+
+// fade in cards
+const showCardsOnScroll = new IntersectionObserver(function (
+  entries,
+  showCardsOnScroll
+) {
+  entries.forEach((entry) => {
+    //   check if entry is intersecting
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      showCardsOnScroll.unobserve(entry.target);
+    }
+  });
+},
+cardOptions);
+
+// show cards on scroll
+cards.forEach((card) => {
+  showCardsOnScroll.observe(card);
+});
 
 // add event listner to the form button
 form.addEventListener('submit', (e) => {
