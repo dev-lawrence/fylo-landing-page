03e49023ce7de383e6ebc98d24030ad42f91e797
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-02-03 15:41:32 +0100

    added animation file and finished the styling the mobile view

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const form = document.querySelector('.form');
 const userEmail = document.querySelector('.email');
 const formBtn = document.querySelector('.form-btn');
+const upBtn = document.querySelector('.up-btn');
 
 // function to submit form
 const submitForm = () => {
   const errorMsg = form.querySelector('.error-msg');
-  // check if field is empty
-  //   if (userEmail.value === '') {
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-  //   } else {
-  //     console.log('false');
-  //   }
-  console.log(errorMsg);
+  // check if the values entered matches the pattern above
+  if (userEmail.value.match(pattern)) {
+    userEmail.value = '';
+  }
+
+  // if it does not match the pattern
+  else {
+    errorMsg.textContent = 'Please enter a valid email address';
+    errorMsg.classList.add('clr');
+    setTimeout(() => (errorMsg.textContent = ''), 3000);
+  }
+};
+
+// show up button
+const showTopBtn = () => {
+  const scroll = window.scrollY;
+
+  if (scroll > 2000) {
+    upBtn.classList.add('toTopBtn');
+  } else {
+    upBtn.classList.remove('toTopBtn');
+  }
+};
+
+// go to top
+const goToTop = () => {
+  window.scrollTo(0, 0);
 };
 
 // add event listner to the form button
@@ -19,3 +42,5 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 formBtn.addEventListener('click', submitForm);
+window.addEventListener('scroll', showTopBtn);
+upBtn.addEventListener('click', goToTop);
